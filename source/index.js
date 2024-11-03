528057fb0645f7a424d7bd5227947a8f4c39235d
JohannaGam <164930877+JohannaGam@users.noreply.github.com>
2024-11-03 21:45:38 +0100

    added typewriter plugin, basic js,

--- source/index.js
+++ source/index.js
@@ -1,1 +1,13 @@
-alert("Hello world");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "Vanitas",
+    autoStart: true,
+    delay: 3,
+    cursor: "",
+  });
+}
+
+let poemForm = document.querySelector("#generator-form");
+poemForm.addEventListener("submit", generatePoem);
