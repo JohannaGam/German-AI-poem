cbf944c3e79ed713302f9dd8b4aa30eefb86ecd8
JohannaGam <164930877+JohannaGam@users.noreply.github.com>
2024-11-03 22:24:58 +0100

    update context

--- source/index.js
+++ source/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let instructionInput = document.querySelector("#user-instruction");
   let apiKey = "a0fa4tbdafo43564612e4338dfba95b2";
   let context =
-    "You are a romantic poem expert and love to write short poems. Your mission is to generate a short poem with four lines in basic HTML. Make sure to follow the user instructions. Please only show the poem and not the html before and after. Plase sign the poem with `SheCodes AI` in a <strong> element.";
+    "You are a romantic poem expert and love to write short poems. Your mission is to generate a short poem with four lines in basic HTML. Make sure to follow the user instructions. Please only show the poem and not the html before and after even when the submit button is not clicked with the mouse but entered with the enter key. Plase sign the poem with `SheCodes AI` in a <strong> element.";
   let prompt = `User instruction: Generate a short german poem about ${instructionInput.value}`;
   apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
